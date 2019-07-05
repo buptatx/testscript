a7d22a9cf96c621bd54f3deecd1b6fd73ea84f2e
zhangpeng <zhp@chinabr.net>
2019-07-05 14:21:40 +0800

    添加point类 成员： 1.x轴坐标 2.y轴坐标 方法： 1.move_to 2.move_by 3.获取x轴坐标 4.获取y轴坐标 5.计算到其他点的距离

--- python100days/scripts/8.py
+++ python100days/scripts/8.py
@@ -1,5 +1,6 @@
 #！ -*- coding:utf-8 -*-
 
+import math
 a = 257
 
 def test_int():
@@ -11,7 +12,39 @@ def test_int():
 
 
 class Point():
-    
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def move_to(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move_by(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def distance_to(self, other_point):
+        dx = self.x - other_point.get_x()
+        dy = self.y - other_point.get_y()
+        dis = math.sqrt(dx**2 + dy**2)
+        return dis
+
+
+def test_point():
+    p1 = Point(3, 5)
+    p2 = Point()
+    p2.move_to(-1, 2)
+    dis = p1.distance_to(p2)
+    print(dis)
+
 
 if __name__ == "__main__":
-    test_int()
+    #test_int()
+    test_point()
